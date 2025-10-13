f3d8c33ec80ab1818e2954194868d0bc669784a6
Albert Alan-Reys <albert.alanreys@gmail.com>
2025-10-13 19:46:15 +1000

    Refactor styles and animations

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 (function initScrollAnimations() {
   ScrollSmoother.create({
-    smooth: 0.8,
+    smooth: 1,
     effects: true,
     smoothTouch: 0.1,
   });
@@ -14,8 +14,23 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
   gsap.from('.fade-in', {
     y: 20,
     opacity: 0,
-    duration: 0.6,
+    duration: 0.8,
     stagger: 0.1,
     ease: 'power2.out',
+  });
+
+  gsap.set('.zoom-in-fade-in', {
+    transformOrigin: 'center center',
+    willChange: 'transform, opacity',
+    backfaceVisibility: 'hidden',
+  });
+
+  gsap.from('.zoom-in-fade-in', {
+    scale: 0.8,
+    opacity: 0,
+    duration: 1.2,
+    ease: 'power3.out',
+    stagger: 0.15,
+    onComplete: () => gsap.set('.zoom-in-fade-in', { willChange: 'auto' }),
   });
 })();
